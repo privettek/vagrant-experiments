2531758d75444f7e50ebd82097026e101dd62f4b
Keith Privette <privette.keith@gmail.com>
2016-07-02 19:31:19 -0700

    yarp

--- vagrant-java/bootstrap.sh
+++ vagrant-java/bootstrap.sh
@@ -11,6 +11,25 @@ export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64
 echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64" >> ~/.bashrc
 ./run.sh -start 8090
 
+sudo apt-get update
+
+echo "Installing Apache.."
+sudo apt-get install -y apache2
+
+echo "Installing Tomcat.."
+sudo apt-get install -y tomcat8
+
+echo "Installing Tomcat8 docs.."
+sudo apt-get install -y tomcat8-docs
+
+echo "Installing Tomcat8 examples.."
+sudo apt-get install -y tomcat8-examples
+
+echo "Installing Tomcat8 administration webapps.."
+sudo apt-get install -y tomcat8-admin
+
+service tomcat8 start
+
 
 
 
--- vagrant-tomcat/vagrantProvision.sh
+++ vagrant-tomcat/vagrantProvision.sh
@@ -3,11 +3,11 @@
     echo "Installing Apache.."
 	sudo apt-get install -y apache2
     echo "Installing Tomcat.."
-	sudo apt-get install -y tomcat7
-	echo "Installing Tomcat7 docs.."
-	sudo apt-get install -y tomcat7-docs
-	echo "Installing Tomcat7 examples.."
-	sudo apt-get install -y tomcat7-examples
-	echo "Installing Tomcat7 administration webapps.."
-	sudo apt-get install -y tomcat7-admin
-	service tomcat7 start
+	sudo apt-get install -y tomcat8
+	echo "Installing Tomcat8 docs.."
+	sudo apt-get install -y tomcat8-docs
+	echo "Installing Tomcat8 examples.."
+	sudo apt-get install -y tomcat8-examples
+	echo "Installing Tomcat8 administration webapps.."
+	sudo apt-get install -y tomcat8-admin
+	service tomcat8 start
